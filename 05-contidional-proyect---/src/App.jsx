3571a91e6feadeb90b90632b8e8b87b14b908d3f
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-08-26 19:52:07 -0600

    agregue

--- 05-contidional-proyect---/src/App.jsx
+++ 05-contidional-proyect---/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react'
 import './App.css'
+import Widget from './components/Widget/Widget'
 
-function App() {
+function App () {
+  // Estado para saber si el usuario ha iniciado sesión o no
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // Funciones para SIMULAR el inicio y cierre de sesión
+  const handleLogin = () => setIsLoggedIn(true)
+
+  const handleLogout = () => setIsLoggedIn(false)
 
   return (
     <div className='container'>
       <h1>Mi Panel de Control</h1>
+
+      {/* --- RENDERIZADO CONDICIONAL CON OP. TERNARIO --- */}
+      {
+        isLoggedIn
+          ? (
+            <Widget title='Perfil de Usuario'>
+              <p><strong>Usuario:</strong> César Guerra</p>
+              <p><strong>Rol:</strong> Administrador</p>
+              <button onClick={handleLogout}>Cerrar Sesión</button>
+            </Widget>
+            )
+          : (
+            <Widget title='Iniciar Sesión'>
+              <p>Por favor, inicia sesión para acceder a tu cuenta.</p>
+              <button onClick={handleLogin}>Iniciar Sesión</button>
+            </Widget>
+            )
+      }
+
     </div>
   )
 }
